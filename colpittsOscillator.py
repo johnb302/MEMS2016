8fe5200e11ab72ee11ffe3162c916af46eccd671
John <jjansonstoughton@outlook.com>
2024-12-12 16:36:45 -0500

    Adding files for project 3. Added numerical integration for project 2

--- colpittsOscillator.py
+++ colpittsOscillator.py
@@ -2,7 +2,10 @@ import numpy as np
 from scipy.integrate import quad
 from scipy.optimize import fsolve
 from scipy.integrate import solve_ivp
+from scipy.interpolate import interp1d
+from scipy.signal import hilbert, find_peaks
 import matplotlib.pyplot as plt
+
 
 def g1(phi0, x, params):
     U, G, H, X, Y = x
@@ -57,6 +60,26 @@ def odefun(t, A0, params):
     return (-1 / 2) * (lambda_ * A0 - H)
 
 
+# Define the nonlinear drain current
+def g(v, u):
+    delta = v - u + 1
+    return gamma * delta**2 if delta >= 0 else 0
+
+
+# Define the first-order ODEs
+def ode9a(t, y):
+    f1, f2, v, u = y  # State variables
+    # Dynamics of v and u
+    u_prime = (g(v, u) - a2 * u) / (1 + mu)
+    v_prime = u_prime - a1 * v - ep * f1
+
+    # Dynamics of f
+    f1_prime = f2
+    f2_prime = -ep**2 * lambda_ * f2 - f1 + ep * v_prime
+
+    return [f1_prime, f2_prime, v_prime, u_prime]
+
+
 # Solution driver function
 if __name__ == "__main__":
     # Dimensional parameters
@@ -88,12 +111,42 @@ if __name__ == "__main__":
         'a0cur': 0, 'nsolve_ics': None
     }
 
-    # Integrate the ODE
-    results = solve_ivp(odefun, [0, 200], [0], args=(params,))
+    t_span = (0, 200)
+    t_eval = np.linspace(*t_span, 50000)
 
+    # Integrate the ODE
+    results = solve_ivp(odefun, t_span, [0], t_eval=t_eval, args=(params,))
+    solution = solve_ivp(ode9a, t_span, [-100, 0, 100, 100], t_eval=t_eval, method='RK45')
+
+    signal = solution.y[0]
+    upperEnvelope = np.zeros(len(signal))
+    # Prepend the first value of (s) to the interpolating values. This forces the model to use the same starting point for both the upper and lower envelope models.
+    u_x = [0, ]
+    u_y = [signal[0], ]
+
+    # Detect peaks and troughs and mark their location in u_x,u_y,l_x,l_y respectively.
+    for k in range(1, len(signal) - 1):
+        if (np.sign(signal[k] - signal[k - 1]) == 1) and (np.sign(signal[k] - signal[k + 1]) == 1):
+            u_x.append(k)
+            u_y.append(signal[k])
+
+    # Append the last value of (s) to the interpolating values. This forces the model to use the same ending point for both the upper and lower envelope models.
+    u_x.append(len(signal) - 1)
+    u_y.append(signal[-1])
+
+    # Fit suitable models to the data. Here I am using cubic splines, similarly to the MATLAB example given in the question.
+    u_p = interp1d(u_x, u_y, kind='cubic', bounds_error=False, fill_value=0.0)
+
+    # Evaluate each model over the domain of (s)
+    for k in range(0, len(signal)):
+        upperEnvelope[k] = u_p(k)
+
+    t = results.t
     # Plot the results
-    plt.plot(results.t / ep ** 2, np.abs(results.y[0]), color='r', linestyle='--', label='$A_0$')
+    plt.plot(t[:25000] / ep ** 2, np.abs(results.y[0])[:25000], color='r', linestyle='--', label='$A_0$')
+    plt.plot(t[:25000] / ep ** 2, upperEnvelope[:25000], color='b', label='||f||')
     plt.xlabel('t/ep^2')
     plt.ylabel('|A0|')
     plt.title('Dynamics of A0 over Non-Dimensional Time')
+    plt.legend()
     plt.show()
